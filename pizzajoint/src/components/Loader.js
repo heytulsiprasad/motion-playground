4fffc3ceb5d1c724f226696418af5a0f8770d060
Tulsi Prasad <tulsi.prasad50@gmail.com>
2023-10-06 09:42:12 +0000

    add loader animation

--- pizzajoint/src/components/Loader.js
+++ pizzajoint/src/components/Loader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { motion } from 'framer-motion';
+import { motion, useCycle } from 'framer-motion';
 
 const loaderVariants = {
   animationOne: {
@@ -16,16 +16,39 @@ const loaderVariants = {
         ease: 'easeOut'
       }
     }
+  },
+  animationTwo: {
+    y: [0, -40],
+    x: 0,
+    transition: {
+        y: {
+            yoyo: Infinity,
+            duration: 0.25,
+            ease: "easeOut"
+        }
+    }
+  },
+  animationThree: {
+    y: [0, -40, -40, 0, 0],
+    x: [0, 0, 40, 40, 0],
+    transition: {
+        yoyo: Infinity,
+        duration: 0.5
+    }
   }
 };
 
 const Loader = () => {
+    const [animation, cycleAnimation] = useCycle("animationOne", "animationTwo", "animationThree")
+
+
   return (
     <>
       <motion.div className="loader"
         variants={loaderVariants}
-        animate="animationOne"
+        animate={animation}
       ></motion.div>
+      <div onClick={() => cycleAnimation()}>Cycle loader</div>
     </>
   )
 }
